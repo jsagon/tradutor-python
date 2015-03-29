c432c5a136b30900d5664e0b133ad1b5c25bc7fe
Jhonatan dos S. Gonçalves <jhonatanrj180@hotmail.com>
2015-03-29 11:38:53 -0300

    Create iniciar.py

---
+++ iniciar.py
@@ -0,0 +1,14 @@
+#coding: utf-8
+
+#  
+#  @JSagon - Jhonatan dos S. Gonçalves
+#  http://www.jsagon.com
+#
+#  Classe principal - Inicialização do programa
+#  Versão 1.0
+#
+
+
+import programa_controller as prog
+
+programa = prog.Programa()
